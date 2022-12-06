2ffacb52c0b4c0e14629468abb9663b0fc6af8d9
Ramesh kumar shah <37567180+shah-ramesh4321@users.noreply.github.com>
2022-12-06 14:20:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "./styles.css";
+import Test from "./Test";
+
+export default function App() {
+  let data = {
+    science: ["Biology", "Physics"],
+    commerce: ["Account"],
+    art: ["Drawing"]
+  };
+  const [showSubject, setShowSubject] = useState(false);
+  const [selectedSubject, setSelectedSubject] = useState(false);
+  const [showNumber, setShowNumber] = useState(false);
+  const handleCourseChange = (e) => {
+    if (e.target.value !== "") {
+      setShowSubject(true);
+      setSelectedSubject(e.target.value);
+    }
+  };
+
+  const handleSubjectChange = (e) => {
+    if (e.target.value !== "") {
+      setShowNumber(true);
+    }
+  };
+  return (
+    <div className="App">
+      <select onChange={(e) => handleCourseChange(e)}>
+        <option value="" selected>
+          Select Subject
+        </option>
+        <option value="science">Science</option>
+        <option value="commerce">Commerce</option>
+        <option value="art">Art</option>
+      </select>
+      {showSubject && (
+        <select onChange={(e) => handleSubjectChange(e)}>
+          <option value="" selected diabled>
+            Select Subject
+          </option>
+          {data[selectedSubject]?.map((item, index) => (
+            <option>{item}</option>
+          ))}
+        </select>
+      )}
+      {showNumber && (
+        <input
+          type="number"
+          placeholder="Please input your mark."
+          min="0"
+          max="100"
+        />
+      )}
+    </div>
+  );
+}
---
+++ src/Test.js
@@ -0,0 +1,5 @@
+const Test = () => {
+  return <h2>Dropdown</h2>;
+};
+
+export default Test();
